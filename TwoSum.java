1d4649bd9484244d8b895781010464e28b4c7e9c
Joel Rego <jrxna@outlook.com>
2024-04-14 16:13:21 +0530

    Refactored the file to improve readability and faster iterations through test cases

--- TwoSum.java
+++ TwoSum.java
@@ -1,17 +1,33 @@
 class TwoSum {
-    public static void main(String args[]) { 
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
+    public static void main(String args[]) {
+        // Create Instances of the TestCase Class
+        TestCase t1 = new TestCase();
+        t1.nums = new int[]{2, 7, 11, 15};
+        t1.target = 9;
+
+        // Create Instance of the Solution Class
+        Solution s = new Solution();
 
         // Call the twoSum function that returns an array of indices
-        int[] result = twoSum(nums, target);
+        t1.result = s.twoSum(t1.nums, t1.target);
 
         // Print the result array to the console
-        System.out.println(result[0]);
-        System.out.println(result[1]);
+        System.out.println(t1.result[0]);
+        System.out.println(t1.result[1]);
     }
+}
 
-    public static int[] twoSum(int[] nums, int target) {
+class TestCase {
+    // TestCase Input
+    public int[] nums;
+    public int target;
+
+    // TestCase Output
+    public int[] result;
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         int [] result = new int[2];
         for(int i = 0; i < nums.length - 1; i++) {
             for(int j = i + 1; j < nums.length; j++) {
